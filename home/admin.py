575f80121fc6d59d483a965fb906626269353c44
vimal-11 <vimalaadith.bs@gmail.com>
2024-03-19 13:00:33 +0530

    import export

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
 from ckeditor.widgets import CKEditorWidget
+from import_export import resources, fields
+from import_export.widgets import ForeignKeyWidget
 from .models import *
+from django.http import HttpResponse
+import xlwt
 
 # Register your models here.
+
+
+class RegistrationExportResource(resources.ModelResource):
+    event_title = fields.Field(attribute='event__title', column_name='Event Title')
+    student_name = fields.Field(attribute='student__name', column_name='Student Name')
+    student_college = fields.Field(attribute='student__college', column_name='Student College')
+    student_email = fields.Field(attribute='student__email__email', column_name='Student Email')
+    student_phone = fields.Field(attribute='student__ph_no', column_name='Contact')
+    student_dept = fields.Field(attribute='student__dept', column_name='Department')
+    student_year = fields.Field(attribute='student__year', column_name='Year')
+    # Include more fields as needed
+
+    class Meta:
+        model = Registration
+        fields = ('event_title', 'student_name', 'student_college', 'student_dept', 'student_year', 'student_phone', 'is_paid', 'registered_at', 'is_reported')
+
+
+def export_selected_to_excel(modeladmin, request, queryset):
+    resource = RegistrationExportResource()
+    dataset = resource.export(queryset)
+
+    response = HttpResponse(dataset.xls, content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="Event_Registration.xls"'
+    return response
+
+export_selected_to_excel.short_description = "Export selected data to Excel"
+
+
+
 
 class CustomUserAdmin(admin.ModelAdmin):
     list_filter = ['email']
@@ -59,6 +92,8 @@ class RegistrationAdmin(admin.ModelAdmin):
     list_filter = ['event', 'student', 'is_paid']
     list_display = ['event', 'student', 'registered_at', 'is_paid', 'is_reported']
     search_fields = ['event__title', 'student__name']
+
+    actions = [export_selected_to_excel]  # Add the custom action
 
     class Meta:
         model = Registration
--- rejouir/settings.py
+++ rejouir/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'phonenumber_field',
     'ckeditor',
     'gunicorn',
+    'import_export',
 ]
 
 MIDDLEWARE = [
